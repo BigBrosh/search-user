1a08cd9598db08c4baa146a849197010952e1bf8
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-03-14 12:03:20 +0200

    UserList added

--- project/scripts/SearchUser.js
+++ project/scripts/SearchUser.js
@@ -1,3 +1,25 @@
 import '../../css/main.css';
 
-console.log(1);
+class UserList {
+	constructor() {
+		this.list = {
+			name: 'Maks',
+			next: null
+		}
+
+		this.showUsers();
+	}
+
+	showUsers() {
+		let current = this.list;
+
+		while (current.next) {
+			console.log(current.name);
+			current = current.next;
+		}
+
+		console.log(current.name);
+	}
+}
+
+let userList = new UserList();
